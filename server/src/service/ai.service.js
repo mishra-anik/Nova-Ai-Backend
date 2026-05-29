7d19b45ee97b486fd7c69563b73e4d23a1d65567
mishra-anik <anikmishra352@gmail.com>
2026-05-29 20:04:42 +0530

    initial error fix

--- server/src/service/ai.service.js
+++ server/src/service/ai.service.js
@@ -4,7 +4,7 @@ const ai = new GoogleGenAI({});
 
 export const generateResponse = async (prompt) => {
 	const response = await ai.models.generateContent({
-		model: "gemini-1.5-pro",
+		model: "gemini-2.5-flash",
 		contents: prompt,
 		config: {
 			temperature: 1.5,
